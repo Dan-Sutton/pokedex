3c7a556bb2bae15de7f9f128cf32af407a5b4740
Dan-Sutton <dewasutton@gmail.com>
2023-02-06 22:54:00 +0000

    pokemodel

--- lib/models/pokeModel.dart
+++ lib/models/pokeModel.dart
@@ -1,10 +1,24 @@
 class Pokemon {
-  int id;
-  String name;
-  String img;
-  Pokemon.fromJson(Map<String, dynamic> data)
-      : id = data['id'],
-        name = data['name'],
-        img = data['img'];
-  Map<String, dynamic> toJson() => {'id': id, 'name': name, 'img': img};
+  final int id;
+  final String name;
+  final String img;
+
+  Pokemon({
+    required this.id,
+    required this.name,
+    required this.img,
+  });
+
+  factory Pokemon.fromJson(dynamic json) {
+    return Pokemon(
+        id: json['is'] as int,
+        name: json['name'] as String,
+        img: json['sprites']['front_default'] as String);
+  }
+
+  static List<Pokemon> pokemonFromSnapshot(List snapshot) {
+    return snapshot.map((data) {
+      return Pokemon.fromJson(data);
+    }).toList();
+  }
 }
